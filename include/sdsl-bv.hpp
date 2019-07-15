44ac3e962a64d4474344de14c7737eaa71475c44
Uula Ulkuniemi <uula.ulkuniemi@outlook.com>
2019-07-15 11:22:51 +0300

    Buckets for SDSL structure

--- include/sdsl-bv.hpp
+++ include/sdsl-bv.hpp
@@ -5,7 +5,6 @@
 #include "util.hpp"
 #include "message.hpp"
 #include <vector>
-#include "select_support_mcl.hpp"
 
 using namespace sdsl;
 using namespace std;
@@ -16,14 +15,14 @@ namespace dyn {
 	public:
 		sdsl_bv() = delete;
 
-		explicit sdsl_bv(uint64_t message_count = 128) {
-			message_buffer = vector<message>();
-			message_buffer.reserve(message_count);
+		explicit sdsl_bv(uint64_t message_count = 0) {
+			bv = bit_vector();
+			util::init_support(rs, &bv);
+			message_buffer = map<uint64_t, pair<uint64_t, vector<message>>>();
+			message_max = message_count;
 		}
 
-		explicit sdsl_bv(uint32_t B = 0, uint32_t B_LEAF = 0, uint64_t message_count = 128) {
-			message_buffer = vector<message>();
-			message_buffer.reserve(message_count);
+		explicit sdsl_bv(uint32_t B = 0, uint32_t B_LEAF = 0, uint64_t message_count = 0) : sdsl_bv(message_count) {
 		}
 
 		~sdsl_bv() = default;
@@ -33,28 +32,20 @@ namespace dyn {
 		}
 
 		bool at(uint64_t i) const {
-			int64_t offset = 0;
-			for (auto y = message_buffer.rbegin(); y != message_buffer.rend(); ++y) {
-				const auto message = *y;
 
-				if (message.get_index() > i) {
-					continue;
-				}
+			uint64_t bucket = i / bucket_width;
 
-				if (message.get_type() == message_type::insert) {
-					if (message.get_index() == i) {
+			auto it = message_buffer.find(bucket);
+
+			if (it != message_buffer.end()) {
+				for (const auto& message : it->second.second) {
+					if ((bucket * bucket_width) + message.get_index() == i) {
 						return message.get_val();
 					}
-					else {
-						--offset;
-					}
-				}
-				else {
-					++offset;
 				}
 			}
 
-			return bv[i + offset];
+			return bv[i];
 		}
 
 		void remove(uint64_t i) {
@@ -62,7 +53,24 @@ namespace dyn {
 		}
 
 		uint64_t size() const {
-			return bv.size() + message_buffer.size();
+			uint64_t counter = 0;
+
+			for (auto const& [index, val] : message_buffer) {
+				for (auto const& message : val.second) {
+					if (message.get_dirty()) {
+						continue;
+					}
+
+					if (message.get_type() == message_type::insert) {
+						++counter;
+					}
+					else if (message.get_type() == message_type::remove) {
+						--counter;
+					}
+				}
+			}
+
+			return bv.size() + counter;
 		}
 
 		void insert(uint64_t i, bool x) {
@@ -79,97 +87,144 @@ namespace dyn {
 		}
 
 		uint64_t select(uint64_t i) {
-			return 0;
+			uint64_t counter = 0;
+			uint64_t index = 0;
+
+			while (counter != i) {
+				if (at(index)) {
+					++counter;
+				}
+				++index;
+			}
+
+			return index + 1;
 		}
 
 		uint64_t rank(const uint64_t i) {
+			uint64_t insertions = 0;
 			uint64_t sum = 0;
-			uint64_t mcount = 0;
 
-			for (auto it = message_buffer.rbegin(); it != message_buffer.rend(); ++it) {
-				if (it->get_index() < i) {
-					++mcount;
-					sum += it->get_val();
+			for (auto const& [bucket, item] : message_buffer) {
+				for (auto const& message : item.second) {
+					if (message.get_dirty()) {
+						continue;
+					}
 
-					if (mcount == i) {
+					if (message.get_index() + (bucket * bucket_width) >= i) {
+						break;
+					}
+
+					++insertions;
+					sum += message.get_val();
+
+					if (insertions == i) {
 						break;
 					}
 				}
-				else {
-					break;
-				}
 			}
 
-			if (rs.initialized()) {
-				sum += rs.rank(i - mcount);
-			}
-
-			return sum;
+			return rs.rank(i - insertions) + sum;
 		}
 
 	private:
 		void flush_messages() {
-			auto inserts = message_buffer.size();
+			vector<uint64_t> temp(size(), -1);
 
-			auto temp = vector(inserts + bv.size(), -1);
+			for (auto const& [bucket, item] : message_buffer) {
+				for (auto const& message : item.second) {
+					if (message.get_dirty()) {
+						continue;
+					}
 
-			for (auto& it = message_buffer.rbegin(); it != message_buffer.rend(); ++it) {
-				auto index = it->get_index();
-
-				while (temp[index] != -1) {
-					++index;
+					temp[message.get_index() + (bucket * bucket_width)] = message.get_val();
 				}
-
-				temp[index] = it->get_val();
 			}
 
-			for (auto i = 0; i < bv.size(); ++i) {
-				auto index = i;
-				while (temp[index] != -1) {
-					++index;
+			for (int i = 0; i < bv.size(); ++i) {
+				if (temp[i] == -1) {
+					temp[i] = bv[i];
 				}
-				temp[index] = bv[i];
+				else {
+					auto counter = i + 1;
+					while (temp[counter] != -1) {
+						++counter;
+					}
+					temp[counter] = bv[i];
+				}
 			}
 
+			message_buffer.clear();
+			messages = 0;
 			bv = bit_vector(temp.size());
 
-			for (int i = 0; i < temp.size(); ++i) {
+			for (auto i = 0; i < temp.size(); ++i) {
 				bv[i] = temp[i];
 			}
 
-			util::init_support(ss, &bv);
 			util::init_support(rs, &bv);
-
-			message_buffer.clear();
 		}
 
 		void add_message(const message& m) {
-			assert(!m.get_dirty());
+			// Calculate bucket and index inside bucket
+			auto bucket = m.get_index() / bucket_width;
+			auto index = m.get_index() % bucket_width;
 
-			if (message_buffer.empty()) {
-				message_buffer.emplace_back(m);
-			}
-			else {
-				for (auto it = message_buffer.begin(); it != message_buffer.end(); ++it) {
-					message& current_message = *it;
-					if (current_message.get_index() < m.get_index()) {
-						message_buffer.insert(it, m);
-						break;
+			// Copy for modifying
+			message copy = m;
+
+			// Set index to value inside bucket
+			copy.set_index(index);
+
+			// Try to find bucket
+			auto it = message_buffer.find(bucket);
+
+			if (it != message_buffer.end()) {
+				// Bucket found, increment existing message indices by modifier and push greater indices
+				// than current message to the right by one
+				for (auto& curr_mes : message_buffer[bucket].second) {
+					auto increment = curr_mes.get_index() >= copy.get_index() ? message_buffer[bucket].first + 1 : message_buffer[bucket].first;
+					curr_mes.set_index(curr_mes.get_index() + increment);
+
+					// Overflow, set index to actual value and readd, mark old as dirty
+					if (curr_mes.get_index() >= bucket_width) {
+						curr_mes.set_index(curr_mes.get_index() + (bucket * bucket_width));
+						auto new_mes = curr_mes;
+						// Will create new buckets, if needed
+						add_message(new_mes);
+						curr_mes.set_dirty(true);
 					}
-					else {
-						it->set_index(it->get_index() + 1);
+
+					// Increment higher bucket modifiers
+					for (auto& [index, val] : message_buffer) {
+						if (index > bucket) {
+							++(val.first);
+						}
 					}
 				}
+
+				// Clear modifier
+				message_buffer[bucket].first = 0;
+
+				// Add message to bucket
+				it->second.second.push_back(copy);
+			}
+			else {
+				// Message not found, create new bucket and a modifier
+				message_buffer.insert({ bucket, { 0, { copy } } });
 			}
 
-			if (message_buffer.size() == message_buffer.capacity()) {
+			++messages;
+
+			if (messages == message_max) {
 				flush_messages();
 			}
 		}
 
-		select_support_mcl<1, 1> ss;
+		uint64_t messages = 0;
+		uint64_t message_max;
+		uint64_t bucket_width = 4;
+		map<uint64_t, pair<uint64_t, vector<message>>> message_buffer;
 		rank_support_v5<1, 1> rs;
 		bit_vector bv;
-		vector<message> message_buffer;
 	};
 }
--- profiler/profiler.cpp
+++ profiler/profiler.cpp
@@ -8,15 +8,12 @@ using namespace dyn;
 
 int main()
 {
-	auto const count = 100;
+	auto const count = 1000000;
 
-	auto tree = new sdsl_bv(0, 0, 10);
+	auto tree = new sdsl_bv(uint64_t(256));
 
 	for (uint64_t i = 0; i < count; i++) {
-		assert(tree->size() == i);
 		tree->insert(i, i % 2);
-		assert(tree->size() == i + 1);
-		assert(tree->at(i) == i % 2);
 	}
 
 	for (uint64_t i = 0; i < count; i++) {
@@ -27,23 +24,17 @@ int main()
 		tree->rank(i + 1);
 	}
 
-	for (uint64_t i = 0; i < count; i++) {
-		tree->select(i);
-	}
+	//for (uint64_t i = 0; i < count; i++) {
+	//	tree->select(i);
+	//}
 
-	for (uint64_t i = 0; i < count; i++) {
-		assert(tree->at(i) == i % 2);
-		auto size = tree->size();
-		tree->set(i, (i + 1) % 2);
-		assert(tree->at(i) == (i + 1) % 2);
-		assert(size == tree->size());
-	}
+	//for (uint64_t i = 0; i < count; i++) {
+	//	tree->set(i, (i + 1) % 2);
+	//}
 
-	for (uint64_t i = 0; i < count; i++) {
-		int size = tree->size();
-		tree->remove(0);
-		assert(size == tree->size() + 1);
-	}
+	//for (uint64_t i = 0; i < count; i++) {
+	//	tree->remove(0);
+	//}
 
 	delete tree;
 }
